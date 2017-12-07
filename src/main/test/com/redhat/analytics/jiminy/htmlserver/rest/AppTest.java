36ba318197ba25914e0a7ba5ea60fa4857e18a89
Zak Hassan <zak.hassan1010@gmail.com>
2017-12-07 10:26:52 -0500

    Fixing ui issue

---
+++ src/main/test/com/redhat/analytics/jiminy/htmlserver/rest/AppTest.java
@@ -0,0 +1,14 @@
+package com.redhat.analytics.jiminy.htmlserver.rest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
