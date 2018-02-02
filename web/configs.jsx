7794cb6cd0ff204830037922c63d1a25eac59942
Peter MacKinnon <pmackinn@redhat.com>
2018-02-02 11:44:08 -0500

    First cut at web refactor/simplification

--- web/configs.jsx
+++ web/configs.jsx
@@ -17,7 +17,7 @@ export const NAVBAR_CONFIG = {
   ],
   titleSrc: {
     path: 'assets/img/brand.svg',
-    alt: 'Radanalytics Spark Mlib MovieRec'
+    alt: 'radanalytics.io Spark Mllib Movie Recommender'
   }
 };
 
